927441c25d4a93157243e7cd97a08a12ae3fefd1
Dim <dimcegnlm@gmail.com>
2023-12-21 14:30:52 +0200

    added the sql querries and the report3 folder

---
+++ 2307_2325_queries.sql
@@ -0,0 +1,30 @@
+-- i
+SELECT COUNT(*) 
+FROM entities_2307_2325; 
+
+
+-- ii
+SELECT country_name, COUNT(*) AS officer_count
+FROM officers_2307_2325
+GROUP BY country_name
+ORDER BY COUNT(*) DESC
+LIMIT 20;
+
+-- iii
+SELECT o.officer_id, o.name, a.address
+FROM officers_2307_2325 o
+LEFT JOIN officers_addresses_2325 oa ON o.officer_id = oa.officer_id
+LEFT JOIN addresses_2307_2325 a ON oa.address_id = a.address_id
+WHERE o.country_name = 'Greece';
+
+
+-- iv
+SELECT DISTINCT o.officer_id, o.name, a.address FROM officers_2307_2325 o 
+LEFT JOIN officers_roles_entities_2307_2325 ore ON o.officer_id = ore.officer_id
+LEFT JOIN entities_2307_2325 e ON ore.entity_id = e.entity_id
+LEFT JOIN entities_addresses_2307_2325 ea ON e.entity_id = ea.entity_id
+LEFT JOIN addresses_2307_2325 a ON ea.address_id = a.address_id
+WHERE e.country_name = 'Greece' AND e.jurisdiction_description = 'Seychelles';
+
+-- v
+ 
