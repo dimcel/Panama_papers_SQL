e3d632ce3a39629363b5091f550d86c9236abe83
Dim <dimcegnlm@gmail.com>
2023-12-22 03:10:06 +0200

    added most of hte q in report3

--- 2307_2325_queries.sql
+++ 2307_2325_queries.sql
@@ -27,4 +27,18 @@ LEFT JOIN addresses_2307_2325 a ON ea.address_id = a.address_id
 WHERE e.country_name = 'Greece' AND e.jurisdiction_description = 'Seychelles';
 
 -- v
- 
+SELECT o.officer_id, o.name, a.address FROM officers_2307_2325 o
+LEFT JOIN officers_addresses_2307_2325 oa ON o.officer_id = oa.officer_id
+LEFT JOIN addresses_2307_2325 a ON oa.address_id = a.address_id
+WHERE o.officer_id = 12156293;
+
+
+UPDATE addresses_2307_2325 a1
+SET address = 'Old Palace, Athens 105 57, Greece'
+WHERE a1.address_id IN (
+    SELECT a2.address_id
+    FROM officers_2307_2325 o
+    LEFT JOIN officers_addresses_2307_2325 oa ON o.officer_id = oa.officer_id
+    LEFT JOIN addresses_2307_2325 a2 ON oa.address_id = a2.address_id
+    WHERE o.officer_id = 12156293
+);
